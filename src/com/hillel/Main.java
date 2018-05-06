24e461904b22b43fb182bed6467caa3bec403f66
Inguarus <35971740+Inguarus@users.noreply.github.com>
2018-05-06 23:47:23 +0300

    create method `readFromFile`

--- src/com/hillel/Main.java
+++ src/com/hillel/Main.java
@@ -1,10 +1,14 @@
 package com.hillel;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.util.Arrays;
+import java.util.Scanner;
+
 
 public class Main {
 
@@ -12,14 +16,38 @@ public class Main {
     static String input;
     static String output;
 
-    public static void main(String[] args) {
-        // parse "settings.txt"
-        // clean "urls.txt" and "result.txt"
+
+    public static void main(String[] args) throws IOException {
+
+        readFromFile();
         downloadFileFromURL(url, input);
         // write "urls.txt"
         // create list only with domains
         // find the most frequent domains and write "result.txt"
     }
+
+    public static void readFromFile() throws IOException {
+        File file = new File("settings.txt");
+        Scanner scanner = new Scanner(file);
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] arrays = line.split("=");
+            System.out.println(Arrays.toString(arrays));
+
+            if (arrays[0].equals("input")) {
+                input = arrays[1];
+            }
+            if (arrays[0].equals("output")) {
+                output = arrays[1];
+            }
+            if (arrays[0].equals("url")) {
+                url = arrays[1];
+            }
+        }
+        scanner.close();
+    }
+
 
     public static void downloadFileFromURL(String path, String file) {
         try {
